94e3997f4288cfd18d171e0b0dd594c35694a811
hajune <buea486@gmail.com>
2026-01-18 20:57:38 +0900

    prettier fix

--- src/store/todos.ts
+++ src/store/todos.ts
@@ -1,6 +1,6 @@
-import {create} from "zustand";
-import {combine} from "zustand/middleware";
-import {immer} from "zustand/middleware/immer";
+import { create } from "zustand";
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 import type { Todo } from "@/types";
 
 const initialState: {
@@ -13,25 +13,25 @@ const useTodosStore = create(
     immer(
         combine(initialState, (set) => ({
             actions: {
-                createTodo: (content:string) => {
-                    set((state) =>{
+                createTodo: (content: string) => {
+                    set((state) => {
                         state.todos.push({
                             id: new Date().getTime(),
                             content: content
                         })
-                    }) 
+                    })
                 },
                 deleteTodo: (targetId: number) => {
                     set((state) => {
                         state.todos = state.todos.filter((todo) => todo.id !== targetId)
-                    })
+                    });
                 },
             }
         }))
     )
 )
 
-export const useTodos = () =>{
+export const useTodos = () => {
     const todos = useTodosStore((store) => store.todos)
     return todos;
 }
