708fc11f6cd86c840e9485f1d853e17825b8bcaa
hajune <buea486@gmail.com>
2026-01-20 23:30:47 +0900

    add: 캐싱 추가

--- src/pages/todo-detail-page.tsx
+++ src/pages/todo-detail-page.tsx
@@ -1,3 +1,7 @@
+import { useParams } from "react-router-dom";
+
 export default function TodoDetailPage() {
+    const params = useParams();
+    const id = params.id();
     return <div>TodoDetailPage</div>;
 }
